33f8c8eedbe130e197bf73886741f217a9e4adba
emrekgn <emrekaganakkaya@gmail.com>
2020-10-01 08:38:50 +0300

    feat: Created nexessary React components

---
+++ src/Book.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BookShelfChanger from './BookShelfChanger';
+
+const Book = props => {
+  const { book } = props
+
+  return (
+    <div className="book">
+        <div className="book-top">
+          <div className="book-cover" 
+              style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}>
+          </div>
+          <BookShelfChanger />
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors.join(", ")}</div>
+    </div>
+  )
+};
+
+Book.propTypes = {
+  book: PropTypes.object.isRequired,
+};
+
+export default Book;
---
+++ src/BookShelf.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Book from './Book';
+
+const BookShelf = props => {
+  const { name, books } = props
+
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{name}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          { books.map((book) => (
+            <li key={book.id}>
+                <Book book={book} />
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  )
+};
+
+BookShelf.propTypes = {
+  name: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+};
+
+export default BookShelf;
